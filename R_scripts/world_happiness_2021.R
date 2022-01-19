d87aace037d529274bfec9c38a14be49f6930053
Jewel Johnson <jeweljohnsonj@gmail.com>
2022-01-19 20:05:54 +0530

    world map

---
+++ R_scripts/world_happiness_2021.R
@@ -0,0 +1,80 @@
+# Read this shape file with the sf library.
+# since {rgdal} is retiring I am using {sf}
+
+#install.packages("sf")
+library(sf)
+world_spdf <- st_read(paste0(getwd(),"/shp/TM_WORLD_BORDERS_SIMPL-0.3.shp"), stringsAsFactors = FALSE)
+
+# load the dataset which you have downloaded
+# please change the location accordingly
+hap_pre <- read.csv(paste0(getwd(),"/datasets/world-happiness-report-2021.csv"))
+
+# renaming column names of ease of use
+colnames(hap_pre)[1] <- "country"
+colnames(hap_pre)[3] <- "score"
+
+# selecting country and score columns
+hap <- hap_pre %>% select(country,score)
+# assigning ranks based on ladder score
+hap <- hap %>% mutate(rank = rank(-score))
+
+# checking which country names are a mismatch between map data and the downloaded dataset
+# this is an important check as we have to join the happiness dataset and shp file with country names
+anti_join(hap, world_spdf,  by = c("country" = "NAME"))
+
+# correcting country names, note that some countries are not available in the shp file
+# the list below are the ones that are available in the shp file
+correct_names <- c("Taiwan Province of China" = "Taiwan",
+                   "South Korea" = "Korea, Republic of",
+                   "Moldova"  = "Republic of Moldova",
+                   "Hong Kong S.A.R. of China"= "Hong Kong",
+                   "Vietnam" = "Viet Nam",
+                   "Congo (Brazzaville)" = "Congo",
+                   "Laos" = "Lao People's Democratic Republic",
+                   "Iran" = "Iran (Islamic Republic of)",
+                   "Palestinian Territories" = "Palestine",
+                   "Myanmar" = "Burma",
+                   "Tanzania" = "United Republic of Tanzania")
+
+# recoding country names 
+hap2 <- hap %>% mutate(country = recode(country, !!!correct_names))
+
+# the command below shows which countries are not joined
+# unfortunately we cannot add these countries unless we change the shp file
+# for now let us omit these countries
+anti_join(hap2, world_spdf,  by = c("country" = "NAME"))
+
+# joining shp file and the happiness data
+world_hap <-  left_join(world_spdf, hap2, by = c("NAME" = "country"))
+
+fill_col <- colorNumeric(palette="viridis", domain=world_hap$score, na.color="transparent")
+
+# Prepare the text for tooltips:
+text <- paste(
+  "Country: ", world_hap$NAME,"<br/>", 
+  "Score: ", world_hap$score, "<br/>", 
+  "Rank: ", world_hap$rank, 
+  sep="") %>%
+  lapply(htmltools::HTML)
+
+#install.packages("leaflet")
+library(leaflet)
+
+# plotting interactive map
+leaflet(world_hap) %>% 
+  addTiles()  %>% 
+  setView( lat=10, lng=0 , zoom=2) %>%
+  addPolygons( 
+    fillColor = ~fill_col(score), 
+    stroke=TRUE, 
+    fillOpacity = 0.9, 
+    color="white", 
+    weight=0.3,
+    label = text,
+    labelOptions = labelOptions( 
+      style = list("font-weight" = "normal", padding = "3px 8px"), 
+      textsize = "13px", 
+      direction = "auto"
+    )
+  ) %>%
+  addLegend( pal=fill_col, values=~score, opacity=0.9, title = "Score", position = "bottomleft" )
